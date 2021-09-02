8bac0df17872144cf00bb6b016283ae22dc663d1
Daniel <dannyblaze469@gmail.com>
2021-09-02 10:38:34 -0400

    commit tree bfbf7577bef104b18d44440112f7648dd866d407

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, length: { is:10 }
+    # validates_length_of :phone_number, :maximum => 10
+    # validates_length_of :phone_number, :minimum => 10
+
+    has_many :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    validate :title_is_clickbait
+
+    belongs_to :author
+
+  def title_is_clickbait
+    errors.add(:title, "Invalid Title") if self.title == "True Facts"
+  end
+
 end
